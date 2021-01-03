49a01e6e2321ad1c5c22f97d1d6258d446ddf703
Ao Dong <aodong99@gmail.com>
2021-01-03 23:52:49 +0000

    auto scroll to today

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,5 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Button, Table } from 'react-bootstrap';
+import './Board.css';
 import Ripple from '../ripple/Ripple.js';
 import { getSettingsByType, updateOneSetting } from '../../services/SettingService.js';
 import { getWorksByMonth, updateOneWork } from '../../services/WorkService.js';
@@ -7,11 +8,16 @@ import { getWorksByMonth, updateOneWork } from '../../services/WorkService.js';
 function Board(props) {
   let now = new Date();
   now = new Date(now.getTime() - now.getTimezoneOffset() * 60000); // offset according to time zone
-  
-  const [month, setMonth] = useState('' + now.getFullYear()
-                            + (now.getMonth() < 9 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1));
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1;
+  const day = now.getDate();
+  const numDays = new Date(year, month, 0).getDate();
+  const tBodyRef = useRef(null);
+  const array31Days = Array.from({length: 31}, (_, i) => {return {day: i + 1}});
+
+  const [yearMonth, setYearMonth] = useState('' + year + (month < 10 ? '0' : '') + month);
   const [workTypes, setWorkTypes] = useState([]);
-  const [works, setWorks] = useState([]);
+  const [works, setWorks] = useState(array31Days);
   const [formData, setFormData] = useState({'workType': ''});
 
   useEffect(() => {
@@ -19,12 +25,22 @@ function Board(props) {
   }, []);
   
   useEffect(() => {
-    fetchWorks(month);
-  }, [month]);
+    fetchWorks(yearMonth, numDays);
+    scrollTo(day);
+  }, [yearMonth, day, numDays]);
+  
+  function scrollTo(day) {
+    var tableBody = tBodyRef.current;
+    tableBody.childNodes[day].scrollIntoView({behavior: 'smooth', block: 'start'});
+  }
   
   async function fetchWorkTypes() {
-    var workTypeSetting = (await getSettingsByType('workTypes')).Items[0];
-    setWorkTypes(workTypeSetting ? workTypeSetting.allUsers : []);
+    try {
+      var allWorkTypes = (await getSettingsByType('workTypes')).Items[0].allUsers;
+      setWorkTypes(allWorkTypes);
+    } catch(err) {
+      setWorkTypes([]);
+    }
   }
   
   async function createWorkType() {
@@ -41,30 +57,41 @@ function Board(props) {
     fetchWorkTypes();
   }
   
-  async function fetchWorks(month) {
-    var res = await getWorksByMonth(month)
-    .catch(err => {
-      console.log(err);
-    });
-    setWorks(res && res.Items ? res.Items : []);
+  async function fetchWorks(yearMonth, numDays) {
+    try {
+      var worksByYearMonth = (await getWorksByMonth(yearMonth)).Items;
+
+      var modifiedWorks = [];
+      for(var i=0, j=0; i<numDays; i++){
+        if(j < worksByYearMonth.length && worksByYearMonth[j].day === i + 1) {
+          modifiedWorks.push(worksByYearMonth[j]);
+          j++;
+        } else {
+          modifiedWorks.push({ day: i+1 });
+        }
+      }
+      setWorks(modifiedWorks);
+    } catch(err) {
+      setWorks([]);
+    }
   }
   
   async function handleClickWork(work, day, workType) {
     if (!props.isSignedin) return;
-    await updateOneWork(month, day, workType);
-    fetchWorks(month);
+    await updateOneWork(yearMonth, day, workType);
+    fetchWorks(yearMonth, numDays);
   }
-  
+
   return (
     <div>
       <h1>My Works App</h1>
       
       <div>
-        Set month -
+        Set year & month -
         <input
-          onChange={e => setMonth(e.target.value)}
-          placeholder="Month"
-          value={month}
+          onChange={e => setYearMonth(e.target.value)}
+          placeholder="year & month"
+          value={yearMonth}
         />
       </div>
       
@@ -78,7 +105,7 @@ function Board(props) {
       </div>
       
       <Table responsive striped bordered hover variant="dark">
-        <thead>
+        <thead className="header">
           <tr>
             <th>Date</th>
             {
@@ -91,15 +118,15 @@ function Board(props) {
             <th>Updated at</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody id="tbody" ref={tBodyRef}>
           {
             works.map(work => (
-              <tr key={work.day}>
-                <td>{month}-{work.day}</td>
+              <tr key={work.day} >
+                <td>{yearMonth}-{work.day}</td>
                 {
                   workTypes.map(workType => (
                     <Ripple customTag="td" className={work[workType] ? "filled" : "empty"} key={workType} onClick={() => handleClickWork(work, work.day, workType)}>
-                      {work[workType]}
+                      {work[workType] ? work[workType].map(username => username + ' ') : ''}
                     </Ripple>
                   ))
                 }
@@ -110,7 +137,7 @@ function Board(props) {
         </tbody>
       </Table>
     </div>
-  )
+  );
 }
 
 export default Board;
