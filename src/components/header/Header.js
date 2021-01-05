a871e98c5530b4def6c2f7c6a29e4337f6e252b3
Ao Dong <aodong99@gmail.com>
2021-01-05 21:53:56 +0000

    added warning for dev env

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,30 @@
+import { useState, useEffect } from 'react';
 // import { Auth } from 'aws-amplify';
 import { Authenticator, SignIn, SignUp, ConfirmSignUp, Greetings } from 'aws-amplify-react';
+import { getEnv } from '../../services/EnvService';
+import { Alert, Button } from 'react-bootstrap';
 import '@aws-amplify/ui/dist/style.css';
-// import { Button } from 'react-bootstrap';
-
 
 function Header(props) {
-  // async function checkUser() {
-  //   Auth.currentAuthenticatedUser()
-  //     .then(user => console.log({ user }))
-  //     .catch(err => console.log(err));
-  //   console.log(await Auth.currentUserInfo());
-  // }
+  const [show, setShow] = useState(false);
+  
+  useEffect(() => {
+    setUpEnvWarning();
+  }, []);
+  
+  async function setUpEnvWarning() {
+    try {
+      const env = (await getEnv()).env;
+      if(env === 'dev') setShow(true);
+    } catch(err) {
+    }
+  }
+  // // async function checkUser() {
+  // //   Auth.currentAuthenticatedUser()
+  // //     .then(user => console.log({ user }))
+  // //     .catch(err => console.log(err));
+  // //   console.log(await Auth.currentUserInfo());
+  // // }
 
   function handleAuthStateChange(state) {
     if (state === 'signedIn') {
@@ -22,6 +36,12 @@ function Header(props) {
 
   return (
     <div>
+      {
+        show ? <Alert variant="warning" onClose={() => setShow(false)} dismissible>
+                    This is the dev page!
+                  </Alert>
+                : null
+      }
       <Authenticator hideDefault={true} onStateChange={handleAuthStateChange} >
           <SignIn/>
           <SignUp/>
