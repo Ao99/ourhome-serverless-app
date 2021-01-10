043e5229f374a8837768a9c9de121793c9207e32
Ao Dong <aodong99@gmail.com>
2021-01-10 17:56:59 +0000

    adjusted footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,30 +1,29 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import { EnvelopeFill, Linkedin, Github } from 'react-bootstrap-icons';
+import './Footer.css';
 
 function Footer() {
   return (
-    <footer>
-      <div className="p-3 my-1 bg-dark text-white">
-        <Container>
-          <Row>
-            <Col>
-              <a href="mailto: aodong99@gmail.com" target="_blank">
-                <EnvelopeFill width="32" height="32" fill="white"/>
-              </a>
-            </Col>
-            <Col>
-              <a href="https://www.linkedin.com/in/aodong" target="_blank">
-                <Linkedin width="32" height="32" fill="white"/>
-              </a>
-            </Col>
-            <Col>
-              <a href="https://github.com/Ao99/ourhome-serverless-app" target="_blank">
-                <Github width="32" height="32" fill="white"/>
-              </a>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+    <footer className="footer p-2 mt-1 bg-dark text-white">
+      <Container>
+        <Row>
+          <Col>
+            <a href="mailto: aodong99@gmail.com" rel="noreferrer" target="_blank">
+              <EnvelopeFill width="32" height="32" fill="white"/>
+            </a>
+          </Col>
+          <Col>
+            <a href="https://www.linkedin.com/in/aodong" rel="noreferrer" target="_blank">
+              <Linkedin width="32" height="32" fill="white"/>
+            </a>
+          </Col>
+          <Col>
+            <a href="https://github.com/Ao99/ourhome-serverless-app" rel="noreferrer" target="_blank">
+              <Github width="32" height="32" fill="white"/>
+            </a>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   );
 }
